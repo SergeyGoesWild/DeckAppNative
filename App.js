a8fa2bae658d0d66fa6a7635fbe03b148db60b2c
Sergey Telnov <sergej.telnov@gmail.com>
2024-02-15 14:23:59 +0100

    Commit before merge

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DecksScreen from './src/screens/DecksScreen';
-import FullSizeImageScreen from './src/screens/FullSizeImageScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import DecksScreen from "./src/screens/DecksScreen";
+import FullSizeImageScreen from "./src/screens/FullSizeImageScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Decks" component={DecksScreen} />
-        <Stack.Screen name="FullSizeImage" component={FullSizeImageScreen} />  
+        <Stack.Screen name="FullSizeImage" component={FullSizeImageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
